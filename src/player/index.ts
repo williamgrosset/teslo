30e8a5d8dc163b06ff270d64bb86b218c3b1c075
William Grosset <williamhgrosset@gmail.com>
2024-01-26 23:29:45 -0700

    Add getters and setters to Player

--- src/player/index.ts
+++ src/player/index.ts
@@ -1,15 +1,31 @@
 import { ELO_SCALE_FACTOR } from './constants'
 
 export class Player {
-  id: string
-  elo: number
+  private _id: string
+  private _elo: number
 
   constructor(id: string, elo: number) {
-    this.id = id
-    this.elo = elo
+    this._id = id
+    this._elo = elo
+  }
+
+  get id(): string {
+    return this._id
+  }
+
+  set id(id: string) {
+    this._id = id
+  }
+
+  get elo() {
+    return this._elo
+  }
+
+  set elo(elo: number) {
+    this._elo = elo
   }
 
   getExpectedResult(opponentElo: number): number {
-    return 1 / (1 + Math.pow(10, (opponentElo - this.elo) / ELO_SCALE_FACTOR))
+    return 1 / (1 + Math.pow(10, (opponentElo - this._elo) / ELO_SCALE_FACTOR))
   }
 }
