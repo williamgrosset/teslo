46825459382ad8d4f9e71e9d064c4c66db203f13
William Grosset <williamhgrosset@gmail.com>
2024-02-11 16:28:14 -0700

    Update match options

--- src/lib/match/options.ts
+++ src/lib/match/options.ts
@@ -1,10 +1,13 @@
 export interface Options {
+  kFactor?: number
   minContestants?: number
   maxContestants?: number
-  kFactor?: number
 }
 
+// Elo calculation
+export const DEFAULT_K_FACTOR = 32
+
+// Min/max contestants
 export const DUEL_SIZE = 2
 export const DEFAULT_MIN_CONTESTANTS = 2
 export const DEFAULT_MAX_CONTESTANTS = 256
-export const DEFAULT_K_FACTOR = 32
