872e566cc091e195e809f49f302a97dc9969be9b
William Grosset <williamhgrosset@gmail.com>
2024-03-09 15:36:01 -0700

    Generate source maps and bundle declaration files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,28 @@
 import typescript from 'rollup-plugin-typescript2'
 import terser from '@rollup/plugin-terser'
+import dts from 'rollup-plugin-dts'
+import del from 'rollup-plugin-delete'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs'
-    },
-    {
-      file: 'dist/index.mjs',
-      format: 'es'
-    }
-  ],
-  plugins: [typescript(), terser()]
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: 'dist/index.mjs',
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    plugins: [typescript({ useTsconfigDeclarationDir: true }), terser()]
+  },
+  {
+    input: 'dist/dts/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts(), del({ hook: 'buildEnd', targets: 'dist/dts' })]
+  }
+]
